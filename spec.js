511378e905713ec9e2101883af0610187103226f
sukhoidragan <47058945+sukhoidragan@users.noreply.github.com>
2019-02-13 17:55:01 +0100

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,1 +1,18 @@
+/* ocekujemo da ce funkcija kojoj prosledimo niz brojeva vratiti samo parne brojeve iz niza*/
 
+describe("Testiranje filter funkcije", () => {
+
+    it("treba da vrati samo parne brojeve", () => {
+       
+       const rezultat = vratiParne([1,2,,4,5,6,7,8,44,33,333,345]);
+       let nadjenNeparni = false;
+       for (let i = 0; i < rezultat.length; i++) {
+           if (rezultat[i] % 2 !== 0) {
+               nadjenNeparni = true;
+               break;
+           }
+       }
+       expect(nadjenNeparni).toBe(false);
+    })
+
+})
